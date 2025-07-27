d1ac9910e29cad9b7ed1d7bfb6b330903d22a5b0
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 02:18:04 +0530

    Create exp6(6).cpp

---
+++ exp6(6).cpp
@@ -0,0 +1,25 @@
+
+//Shivangi Singh
+//24070123148
+//B3
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=6;i>=1;i--){
+        for (int j=1;j<=i;j++){
+          cout<<"* ";}
+          cout<<endl;
+        }
+return 0;
+    }
+
+
+/*
+output:
+* * * * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+*/
