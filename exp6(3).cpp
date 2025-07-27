48930a691a8b043a9afcecb62939abb702680fb1
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 02:11:30 +0530

    Create exp6(3).cpp

---
+++ exp6(3).cpp
@@ -0,0 +1,20 @@
+//Shivangi Singh
+//24070123148
+//B3
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=0;i<5;i++){
+       
+            cout<<i+1<<"."<< "SIT"<<endl;
+       
+    }
+  return 0;
+}
+
+//Output:
+//1.SIT
+//2.SIT
+//3.SIT
+//4.SIT
+//5.SIT
