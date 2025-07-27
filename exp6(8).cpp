683e5e053e20102dea321b42118b37f42958f6e3
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 02:30:18 +0530

    Update exp6(8).cpp

--- exp6(8).cpp
+++ exp6(8).cpp
@@ -1,32 +1,26 @@
 //Shivangi Singh
 //24070123148
 //B3
+
 #include <iostream>
 using namespace std;
-
-int main() {
-    for (int i = 1; i <= 6; i++) {
-        for (int j = 1; j <= 6 - i; j++) {
-            cout << "  ";
+int main (){
+    for (int i=6;i>=1;i--){
+        for (int j=0;j<=i;j++){
+          cout<<" ";}
+         for(int k=6;k>=i;k--){
+          cout<<" *";}
+          cout<<endl;
         }
-        for (int k = 1; k <= i; k++) {
-            cout << "* ";
-        }
-        cout << endl;
+return 0;
     }
-
-    return 0;
-}
-
-
+   
 /*
 output:
-          * 
-        * * 
-      * * * 
-    * * * * 
-  * * * * * 
-* * * * * *
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+   * * * * * *
     */
-
-
