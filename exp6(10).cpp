cd8ebc375760d596a2d3c5e16d2f9df611716fd2
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 02:33:06 +0530

    Create exp6(10).cpp

---
+++ exp6(10).cpp
@@ -0,0 +1,18 @@
+//Shivangi Singh
+//24070123148
+//B3
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 20; i++) {
+       
+            cout << i<<" " ;
+    }
+
+    return 0;
+}
+
+/*  OUTPUT:
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
+    */
