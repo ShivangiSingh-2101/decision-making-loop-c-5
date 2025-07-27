3e6871d56c17c480e591fc07c05758612c428a4f
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 02:09:07 +0530

    Create exp6(1).cpp

---
+++ exp6(1).cpp
@@ -0,0 +1,33 @@
+//Shivangi Singh
+//24070123148
+//B3
+#include <iostream>
+using namespace std;
+
+int main() {
+    string password = "007";
+    int a=0;
+    while(true){
+        cout<< "enter the password : "<<endl;
+        string p;
+        cin>>p;
+        if (p==password){
+            cout<<"correct!!  ";
+            break;
+        }
+        else{
+            cout<<"try again"<<endl;
+        }
+    }
+return 0;
+}
+/*output: 
+enter the password : 
+Sam
+try again
+enter the password : 
+Shampoo
+try again
+enter the password : 
+Sammy
+correct!! */
